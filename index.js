ee51067eec2a09694732c2217feaf86308fd774f
kch <kch19920726@gmail.com>
2021-10-07 11:50:07 +0900

    portfolio 영역 scroll event 수정
    - 스크롤 이벤트 시, 현재 스크롤 위치에 해당하는 요소에 선택적으로 모션 적용
    
    - 스크롤 위로 올렸을 때 해당 모션 리셋
    
    - 기타 스타일 수정

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 const winInnerHeight = window.innerHeight;
 const pageHeight = document.documentElement.offsetHeight;
 const mainSection = document.querySelector('.main-section');
+const portfolioImgBox = document.querySelectorAll('.portfolio-img-list li');
 const card = document.querySelector('footer .card');
 const cardOffsetTop = card.parentElement.offsetTop + card.offsetTop + card.clientHeight;
 const initialAngleValue = 90;
 const percentage = () => resetSct/(pageHeight-cardOffsetTop)*-90;
 
-let mainBackColorR, mainBackColorG, mainBackColorB;
+let mainBackColorR = 0, mainBackColorG = 0, mainBackColorB = 0, effectClassName;
 
-document.addEventListener('mousemove', function(e){
-    let moveX = parseInt(e.x/mainSection.offsetWidth*255);
-    let moveY = parseInt(e.y/mainSection.offsetHeight*255);
-
-    console.log(mainBackColorR);
-    console.log(mainBackColorG);
-    console.log(mainBackColorB);
-
-    mainSection.style.backgroundColor= `${moveX},0,${moveY},.8)`;
-    mainBackColorR = moveX;
-    mainBackColorG = 0;
-    mainBackColorB = moveY;
+window.addEventListener('load', function(e) {
+    setBackgroundColor();
 });
 
-document.addEventListener('scroll', function() {
+mainSection.addEventListener('mousemove', function(e){
+    if(document.documentElement.scrollTop <= 0) {
+        let moveX = parseInt(e.x/mainSection.offsetWidth*255);
+        let moveY = parseInt(e.y/mainSection.offsetHeight*255);
+
+        setBackgroundColor();
+        mainBackColorR = moveX;
+        mainBackColorG = 0;
+        mainBackColorB = moveY;
+    }
+});
+
+document.addEventListener('scroll', function(e) {
     let scrollBottom = document.documentElement.scrollTop + winInnerHeight;
 
-    // main section
-    mainSection.style.backgroundColor= `rgba(${mainBackColorR},${mainBackColorG},${mainBackColorB},${1-(document.documentElement.scrollTop/mainSection.offsetHeight)})`;
-    console.log(1-(document.documentElement.scrollTop/mainSection.offsetHeight));
+    setBackgroundColor();
+
     if(document.documentElement.scrollTop <= 0) {
         mainSection.classList.remove('blow');
     }else{
         mainSection.classList.add('blow');
     }
+    getScrollDirection() == 'DOWN' && portfolioImgBox.forEach((obj,i,arr) => {
+        if(arr[i].offsetTop < scrollBottom - 200){
+            arr[i].classList.add('on');
+        }
+    });
+    getScrollDirection() == 'UP' && portfolioImgBox.forEach((obj,i,arr) => {
+        if(arr[i].offsetTop > scrollBottom){
+            arr[i].classList.remove('on');
+        }
+    });
     
     // footer card
     if(scrollBottom > cardOffsetTop) {
@@ -58,3 +70,17 @@ card.addEventListener('mouseleave', function(){
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     card.style.transition = '1s';
 });
+
+function setBackgroundColor(){
+    document.body.style.backgroundColor= `rgba(${mainBackColorR},${mainBackColorG},${mainBackColorB},${1-(document.documentElement.scrollTop/mainSection.offsetHeight)})`;
+}
+
+// 스크롤 방향 감지
+let scrollBaseValue = 0;
+function getScrollDirection(){
+    let result;
+    if(scrollBaseValue < window.scrollY) result = 'DOWN';
+    else result = 'UP';
+    scrollBaseValue = window.scrollY;
+    return result;
+}
