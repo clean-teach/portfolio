efb20e7e23efe0fa2186e39b930765872fbaff01
kch <kch19920726@gmail.com>
2021-10-07 15:51:08 +0900

    portfolio text area motion function edit
    스크롤에 따른 포트폴리오 글자 영역의 효과 수정

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const winInnerHeight = window.innerHeight;
 const pageHeight = document.documentElement.offsetHeight;
 const mainSection = document.querySelector('.main-section');
 const portfolioImgBox = document.querySelectorAll('.portfolio-img-list li');
+const portfolioTxtBox = document.querySelectorAll('.portfolio-txt-area li');
 const card = document.querySelector('footer .card');
 const cardOffsetTop = card.parentElement.offsetTop + card.offsetTop + card.clientHeight;
 const initialAngleValue = 90;
 const percentage = () => resetSct/(pageHeight-cardOffsetTop)*-90;
 
-let mainBackColorR = 0, mainBackColorG = 0, mainBackColorB = 0, effectClassName;
+let mainBackColorR = 0, 
+    mainBackColorG = 0, 
+    mainBackColorB = 0, 
+    effectClassName, 
+    scrollBottom = document.documentElement.scrollTop + winInnerHeight;
 
 window.addEventListener('load', function(e) {
+    scrollBottom = document.documentElement.scrollTop + winInnerHeight;
+    portfolioImgBox.forEach((obj,i,arr) => {
+        if(arr[i].offsetTop < scrollBottom - 200){
+            arr[i].classList.add('on');
+        }
+        setPortfolioTxtAreaAddClass(i,arr);
+    });
+
     setBackgroundColor();
+    window.scrollBy( 0, 1 );
 });
 
 mainSection.addEventListener('mousemove', function(e){
@@ -26,7 +40,7 @@ mainSection.addEventListener('mousemove', function(e){
 });
 
 document.addEventListener('scroll', function(e) {
-    let scrollBottom = document.documentElement.scrollTop + winInnerHeight;
+    scrollBottom = document.documentElement.scrollTop + winInnerHeight;
 
     setBackgroundColor();
 
@@ -35,16 +49,26 @@ document.addEventListener('scroll', function(e) {
     }else{
         mainSection.classList.add('blow');
     }
-    getScrollDirection() == 'DOWN' && portfolioImgBox.forEach((obj,i,arr) => {
-        if(arr[i].offsetTop < scrollBottom - 200){
-            arr[i].classList.add('on');
-        }
+
+    portfolioTxtBox.forEach((obj) => {
+        obj.classList.remove('on');
     });
-    getScrollDirection() == 'UP' && portfolioImgBox.forEach((obj,i,arr) => {
-        if(arr[i].offsetTop > scrollBottom){
-            arr[i].classList.remove('on');
-        }
-    });
+    if(getScrollDirection() == 'DOWN') {
+        portfolioImgBox.forEach((obj,i,arr) => {
+            if(arr[i].offsetTop < scrollBottom - 200){
+                arr[i].classList.add('on');
+            }
+            setPortfolioTxtAreaAddClass(i,arr);
+        });
+    }
+    if(getScrollDirection() == 'UP') {
+        portfolioImgBox.forEach((obj,i,arr) => {
+            if(arr[i].offsetTop > scrollBottom){
+                arr[i].classList.remove('on');
+            }
+            setPortfolioTxtAreaAddClass(i,arr);
+        });
+    }
     
     // footer card
     if(scrollBottom > cardOffsetTop) {
@@ -56,6 +80,7 @@ document.addEventListener('scroll', function(e) {
     }
 });
 
+// footer card
 const sensitiveY = 40;
 const sensitiveX = 20;
 const direction = 1; // positive or negative
@@ -71,6 +96,17 @@ card.addEventListener('mouseleave', function(){
     card.style.transition = '1s';
 });
 
+// 포트폴리오 글자 영역 나타나는 조건에 대한 함수
+function setPortfolioTxtAreaAddClass(i,arr) {
+    const startPoint = document.documentElement.scrollTop > arr[i].offsetTop - (window.innerHeight/2);
+    const endPoint = scrollBottom < (arr[i].offsetTop + arr[i].offsetHeight) + (window.innerHeight/2);
+
+    if(startPoint && endPoint) {
+        portfolioTxtBox[i].classList.add('on');
+    }
+}
+
+// scroll, mousemove 에 따른 배경색상 설정 함수
 function setBackgroundColor(){
     document.body.style.backgroundColor= `rgba(${mainBackColorR},${mainBackColorG},${mainBackColorB},${1-(document.documentElement.scrollTop/mainSection.offsetHeight)})`;
 }
