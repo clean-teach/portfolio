a19ca55070d1f73af991009a6c76441770548a99
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-08 02:50:24 +0900

    open API key 원상복귀
    API key 숨기는 방법 알아보기

--- portfolio/study/weather-api/weather.js
+++ portfolio/study/weather-api/weather.js
@@ -2,6 +2,7 @@ navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
 
 function onGeoSuccess(position){
     // console.log(position);
+    const API_KEY = '7d0c90c3d6f86da891ab48ecb5a698bf';
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
