5655e6ad0c6d665e1abf370017bf39985f7085df
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-04-03 21:02:37 +0900

    README.md 파일 수정 중..
    1. 목차 설정
    2. 각 목차에 대한 내용 서술 필요..

--- src/client/js/loadingPage.ts
+++ src/client/js/loadingPage.ts
@@ -1,29 +1,33 @@
-(function loadingPage(){
-    let loader: Element | HTMLElement;
-    document.addEventListener('DOMContentLoaded', function () {
-        loader = document.querySelector('#loader') as Element | HTMLElement;
-    });
-    window.addEventListener('load', windowLoadHandler);
-    
-    function windowLoadHandler():void {
-        let opacity = Number(window.getComputedStyle(loader).getPropertyValue('opacity'));
+(function loadingPage() {
+  let loader: Element | HTMLElement;
+  document.addEventListener('DOMContentLoaded', function () {
+    loader = document.querySelector('#loader') as Element | HTMLElement;
+  });
+  window.addEventListener('load', windowLoadHandler);
 
-        window.scrollTo({top: 0, left: 0});
-        setTimeout(function(){
-            let fadeOut = setInterval(() => {
-                if(opacity > 0) {
-                    opacity -= .01;
-                    loader.querySelectorAll('p').forEach(p => {
-                        p.style.opacity = `${opacity}`;
-                    });
-                }else{
-                    clearInterval(fadeOut);
-                }
-            }, 10);
-        }, 3000);
-        setTimeout(function(){
-            window.scrollTo({top: 0, left: 0});
-            loader.remove();
-        }, 5000);
-    }
-}());
+  console.log('힝');
+
+  function windowLoadHandler(): void {
+    let opacity = Number(
+      window.getComputedStyle(loader).getPropertyValue('opacity'),
+    );
+
+    window.scrollTo({ top: 0, left: 0 });
+    setTimeout(function () {
+      let fadeOut = setInterval(() => {
+        if (opacity > 0) {
+          opacity -= 0.01;
+          loader.querySelectorAll('p').forEach((p) => {
+            p.style.opacity = `${opacity}`;
+          });
+        } else {
+          clearInterval(fadeOut);
+        }
+      }, 10);
+    }, 3000);
+    setTimeout(function () {
+      window.scrollTo({ top: 0, left: 0 });
+      loader.remove();
+    }, 5000);
+  }
+})();
