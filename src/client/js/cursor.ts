2df68fab708802a9f90f91172662f3544a867843
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-01-24 23:52:32 +0900

    버튼 비활성화 style 추가
    - CSS 추가
    - 커스텀 마우스 커서 조건 수정
        - 버튼 태그가 disabled 일 경우, 마우스 커서 반응 제거

--- src/client/js/cursor.ts
+++ src/client/js/cursor.ts
@@ -1,20 +1,27 @@
-(function createSetMouseCursorStyle ():void {
-    const ID_NAME_MOUSE_CURSOR:string = 'mouseCursor'
-    const CLASS_NAME_MOUSE_CURSOR_POINTER:string = 'pointer';
-    const mouseCursor:HTMLElement = document.createElement('div');
-    mouseCursor.id = ID_NAME_MOUSE_CURSOR;
-    mouseCursor.innerHTML = '<div><div></div></div>';
-    document.body.appendChild(mouseCursor);
-    document.querySelectorAll('a, button').forEach(tg => {
-        tg.addEventListener('mouseenter',function(e){
-            mouseCursor.classList.add(CLASS_NAME_MOUSE_CURSOR_POINTER);
-        });
-        tg.addEventListener('mouseleave',function(e){
-            mouseCursor.classList.remove(CLASS_NAME_MOUSE_CURSOR_POINTER)
-        });
+(function createSetMouseCursorStyle(): void {
+  const ID_NAME_MOUSE_CURSOR: string = 'mouseCursor';
+  const CLASS_NAME_MOUSE_CURSOR_POINTER: string = 'pointer';
+  const mouseCursor: HTMLElement = document.createElement('div');
+  mouseCursor.id = ID_NAME_MOUSE_CURSOR;
+  mouseCursor.innerHTML = '<div><div></div></div>';
+  document.body.appendChild(mouseCursor);
+  document.querySelectorAll('a, button').forEach((tg) => {
+    tg.addEventListener('mouseenter', function (e) {
+      const targetElement = e.target;
+      console.log();
+      if (
+        targetElement instanceof HTMLButtonElement &&
+        !targetElement?.disabled
+      ) {
+        mouseCursor.classList.add(CLASS_NAME_MOUSE_CURSOR_POINTER);
+      }
     });
-    document.addEventListener('mousemove',function(e){
-        mouseCursor.style['top'] = e.clientY - (mouseCursor.clientHeight/2) + 'px';
-        mouseCursor.style['left'] = e.clientX - (mouseCursor.clientWidth/2) + 'px';
+    tg.addEventListener('mouseleave', function (e) {
+      mouseCursor.classList.remove(CLASS_NAME_MOUSE_CURSOR_POINTER);
     });
-}());
+  });
+  document.addEventListener('mousemove', function (e) {
+    mouseCursor.style['top'] = e.clientY - mouseCursor.clientHeight / 2 + 'px';
+    mouseCursor.style['left'] = e.clientX - mouseCursor.clientWidth / 2 + 'px';
+  });
+})();
